6402b103e824748651a4a07503a65fbb6e793190
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-02-17 19:26:09 +0200

    Add first working prototype

--- main.py
+++ main.py
@@ -2,28 +2,16 @@ import logging
 import os
 import time
 
-import kb as kb
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.markdown import hlink, link
+from aiogram.utils.markdown import hlink
 from dotenv import load_dotenv
 
-from oop_parser import Parser, SingleExactMatch, PageNotAccessible, TermNotFound
+from oop_parser import Parser, PageNotAccessible, TermNotFound, TooManyResults
 
 load_dotenv()
 
 bot = Bot(token=os.environ.get("TOKEN"))
 dp = Dispatcher(bot=bot)
-
-LANGUAGES_KEYBOARD = ReplyKeyboardMarkup(resize_keyboard=True)
-
-LANGUAGES_KEYBOARD.add(KeyboardButton("English 🏴󠁧󠁢󠁥󠁮󠁧󠁿"))
-LANGUAGES_KEYBOARD.add(KeyboardButton("Ukrainian 🇺🇦"))
-LANGUAGES_KEYBOARD.add(KeyboardButton("German 🇩🇪"))
-LANGUAGES_KEYBOARD.add(KeyboardButton("French 🇫🇷"))
-LANGUAGES_KEYBOARD.add(KeyboardButton("Italian 🇮🇹"))
 
 
 @dp.message_handler(commands=["start"])
@@ -40,29 +28,28 @@ async def start_handler(message: types.Message):
         await bot.send_message(chat_id=user_id, text=f"Working... {i}")
 
 
-# @dp.message_handler(commands=['hi1'])
-# async def process_hi1_command(message: types.Message):
-#     await message.reply("Клавиатура1", reply_markup=LANGUAGES_KEYBOARD)
-
-
 @dp.message_handler(commands=["end"])
 async def end_handler(message: types.Message):
     user_id = message.from_user.id
     name = message.from_user.full_name
     logging.info(f"{user_id} {name} {time.asctime()}")
 
-    await message.reply(f"Goodbye!")
+    await message.reply("Goodbye!")
 
 
 @dp.message_handler()
 async def handler(message: types.Message):
     user_id = message.from_user.id
     try:
-        parser = SingleExactMatch(message=message.text)
+        parser = Parser(message=message.text).router()
+        print(parser.__class__.__name__)
         for i in parser.parse():
-            await bot.send_message(chat_id=user_id, text=hlink(i[1][0], i[1][1]), parse_mode="HTML")
-    except (PageNotAccessible, TermNotFound) as e:
+            await bot.send_message(chat_id=user_id,
+                                   text=f"{i[1][0]}\n<i>{hlink(i[0], i[1][1])}</i>",
+                                   parse_mode="HTML")
+    except (PageNotAccessible, TermNotFound, TooManyResults) as e:
         await bot.send_message(chat_id=user_id, text=str(e))
+
 
 if __name__ == "__main__":
     executor.start_polling(dp)
--- oop_parser.py
+++ oop_parser.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-import pprint
 import re
-from abc import ABC, abstractmethod
+from abc import ABC
 
 import requests
 from bs4 import BeautifulSoup
@@ -31,7 +30,19 @@ class TermNotFound(Exception):
         self._term = term
 
     def __str__(self) -> str:
-        return "Sorry, term wasn`t found!"
+        return f"Sorry, {self._term} wasn`t found!"
+
+    @property
+    def term(self) -> str:
+        return self._term
+
+
+class TooManyResults(Exception):
+    def __init__(self, term: str):
+        self._term = term
+
+    def __str__(self) -> str:
+        return f"Sorry, there are too many results for {self._term} found!"
 
     @property
     def term(self) -> str:
@@ -49,20 +60,21 @@ class Parser(ABC):
 
     SOUP_FOR_INITIAL_SEARCH = "ul.m-3 li a"
     SOUP_FOR_RETRIEVING_FROM_TERM = "div.card dl"
+    SOUP_FOR_COUNTING_NUMBER_OF_RESULTS = "h2 small b"
 
     def __init__(self, message: str):
         self.message = message
 
-    def get_url(self):
+    def get_url(self) -> str:
         return self.URL + self.message
 
-    def get_response(self):
+    def get_response(self) -> Response:
         response = requests.get(self.get_url())
         if response.status_code >= 400:
             raise PageNotAccessible(status_code=response.status_code, response=response)
         return response
 
-    def get_url_links(self):
+    def get_url_links(self) -> dict:
         soup = BeautifulSoup(self.get_response().text, "html.parser")
         links = dict()
         if not soup.select(self.SOUP_FOR_INITIAL_SEARCH):
@@ -71,146 +83,43 @@ class Parser(ABC):
             links[i.text] = i.get("href")
         return links
 
-    def exact_match(self):
-        return " ".join([i.capitalize() for i in self.message.split(" ")]) in self.get_url_links().keys()
+    def is_exact_match(self) -> bool:
+        print(list(map(lambda x: x.lower(), self.get_url_links().keys())))
+        return " ".join([i.capitalize() for i in self.message.split(" ")]).lower() \
+               in list(map(lambda x: x.lower(), self.get_url_links().keys()))
 
-    @abstractmethod
     def parse(self):
         pass
 
-        # def get_url(self):
-        #     return self.URL + self.message
-        #
-        # def get_url_response(self) -> Response:
-        #     return requests.get(self.get_url())
-        #
-        # def get_url_soup(self) -> BeautifulSoup:
-        #     return BeautifulSoup(self.get_url_response().text, "html.parser")
-        #
-        # def get_url_links(self):
-        #     return [i.text for i in self.get_url_soup().select("ul.m-3 li a")]
-        #
-        # def exact_match(self):
-        #     return " ".join([i.capitalize() for i in self.message.split(" ")]) in self.get_url_links()
-
-        # def router(self):
-        #     if self.exact_match():
-        #         return CLASS_FOR_EXACT_MATCH
-        #     return CLASS_FOR_MANY_MATCHES
-
-        # @abstractmethod
-        # def factory_method(self):
-        #     """
-        #     Note that the Creator may also provide some default implementation of
-        #     the factory method.
-        #     """
-        #     pass
-        #
-        # def some_operation(self) -> str:
-        #     """
-        #     Also note that, despite its name, the Creator's primary responsibility
-        #     is not creating products. Usually, it contains some core business logic
-        #     that relies on Product objects, returned by the factory method.
-        #     Subclasses can indirectly change that business logic by overriding the
-        #     factory method and returning a different type of product from it.
-        #     """
-
-        # Call the factory method to create a Product object.
-        parser = self.parse()
-
-        # Now, use the product.
-        # result = f"Creator: The same creator's code has just worked with {product.operation()}"
-
-        # return result
+    def router(self):
+        if self.is_exact_match():
+            return ExactMatch(self.message)
+        return PartialMatch(self.message)
 
 
-class SingleExactMatch(Parser):
+class ExactMatch(Parser):
     def parse(self):
         result = dict()
+        response = requests.get(list(self.get_url_links().items())[0][1])
+        soup = BeautifulSoup(response.text, "html.parser")
+        for i in soup.select(self.SOUP_FOR_RETRIEVING_FROM_TERM):
+            result[re.sub(r"\xa0", "", i.select_one("div.doc a").text)] = [re.sub("\n", "", i.select_one("p").text),
+                                                                           i.select_one("div.doc a").get("href")]
+        print(result.items())
+        return result.items()
+
+
+class PartialMatch(Parser):
+    def parse(self):
+        result = dict()
+        soup = BeautifulSoup(self.get_response().text, "html.parser")
+        if int([i.text for i in soup.select(self.SOUP_FOR_COUNTING_NUMBER_OF_RESULTS)][0]) > 10:
+            raise TooManyResults(term=self.message)
         for j in list(self.get_url_links().items()):
             response = requests.get(j[1])
             soup = BeautifulSoup(response.text, "html.parser")
             for i in soup.select(self.SOUP_FOR_RETRIEVING_FROM_TERM):
                 result[re.sub(r"\xa0", "", i.select_one("div.doc a").text)] = [re.sub("\n", "", i.select_one("p").text),
                                                                                i.select_one("div.doc a").get("href")]
+        # print(result.items())
         return result.items()
-
-
-class MultipleExactMatch(Parser):
-    def parse(self):
-        pass
-
-
-"""
-Concrete Creators override the factory method in order to change the resulting
-product's type.
-"""
-
-
-class ConcreteCreator1(Parser):
-    """
-    Note that the signature of the method still uses the abstract product type,
-    even though the concrete product is actually returned from the method. This
-    way the Creator can stay independent of concrete product classes.
-    """
-
-    def factory_method(self) -> Term:
-        return ConcreteProduct1()
-
-
-class ConcreteCreator2(Parser):
-    def factory_method(self) -> Term:
-        return ConcreteProduct2()
-
-
-class Term(ABC):
-    """
-    The Product interface declares the operations that all concrete products
-    must implement.
-    """
-
-    @abstractmethod
-    def operation(self) -> str:
-        pass
-
-
-"""
-Concrete Products provide various implementations of the Product interface.
-"""
-
-
-class ConcreteProduct1(Term):
-    def operation(self) -> str:
-        return "{Result of the ConcreteProduct1}"
-
-
-class ConcreteProduct2(Term):
-    def operation(self) -> str:
-        return "{Result of the ConcreteProduct2}"
-
-
-def client_code(creator: Parser) -> None:
-    """
-    The client code works with an instance of a concrete creator, albeit through
-    its base interface. As long as the client keeps working with the creator via
-    the base interface, you can pass it any creator's subclass.
-    """
-
-    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
-          f"{creator.some_operation()}", end="")
-
-
-if __name__ == "__main__":
-    # print("App: Launched with the ConcreteCreator1.")
-    # test = ConcreteCreator1("посадка")
-    # pprint.pprint(test.get_url_links())
-    # print(test.exact_match())
-    # # print(test.router())
-    # print("\n")
-    #
-    # print("App: Launched with the ConcreteCreator2.")
-    # ConcreteCreator1("Ліонель Мессі").get_url()
-    # test = SingleExactMatch("тривога")
-    # test.parse()
-    test = SingleExactMatch()
-    test.parse()
