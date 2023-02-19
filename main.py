74e38df20d86c00bb1af9578d6f94d7e72f142f6
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-02-19 22:12:46 +0200

    Add inline keyboard markup

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import logging
 import os
+import random
 import time
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.markdown import hlink
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 
 from message_translator import translate_message
@@ -23,11 +25,6 @@ async def start_handler(message: types.Message):
 
     await message.reply(f"Hello, {name}")
 
-    for i in range(5):
-        time.sleep(2)
-
-        await bot.send_message(chat_id=user_id, text=f"Working... {i}")
-
 
 @dp.message_handler(commands=["end"])
 async def end_handler(message: types.Message):
@@ -41,16 +38,28 @@ async def end_handler(message: types.Message):
 @dp.message_handler()
 async def handler(message: types.Message):
     user_id = message.from_user.id
+    keyboard = InlineKeyboardMarkup()
     try:
         parser = Parser(message=translate_message(message.text)).router()
         print(parser.__class__.__name__)
         print(translate_message(message.text))
-        for i in parser.parse():
-            await bot.send_message(chat_id=user_id,
-                                   text=f"{i[1][0]}\n<i>{hlink(i[0], i[1][1])}</i>",
-                                   parse_mode="HTML")
+        if parser.__class__.__name__ == "PartialMatch":
+            for i in parser.parse():
+                keyboard.add(InlineKeyboardButton(text=i[0], callback_data="answer"))
+            await message.answer("Here are the results:", reply_markup=keyboard)
+        else:
+            for i in parser.parse():
+                await bot.send_message(chat_id=user_id,
+                                       text=f"{i[1][0]}\n<i>{hlink(i[0], i[1][1])}</i>",
+                                       parse_mode="HTML")
     except (PageNotAccessible, TermNotFound, TooManyResults) as e:
         await bot.send_message(chat_id=user_id, text=str(e))
+
+
+@dp.callback_query_handler(text=["answer"])
+async def answer(call):
+    await call.message.answer(call)
+    await call.answer(call)
 
 
 if __name__ == "__main__":
