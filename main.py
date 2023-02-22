48431e14e377e90ee34c546df04c7e6b634e470b
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-02-22 16:43:55 +0200

    Add redis

--- main.py
+++ main.py
@@ -78,11 +78,15 @@
 import logging
 import os
 import random
+import sys
 import time
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.redis import RedisStorage
+from aiogram.utils.callback_data import CallbackData
 from aiogram.utils.markdown import hlink
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aioredis import Redis
 from dotenv import load_dotenv
 
 from message_translator import translate_message
@@ -90,8 +94,13 @@ from oop_parser import Parser, PageNotAccessible, TermNotFound, TooManyResults
 
 load_dotenv()
 
+
+redis = Redis()
+
 bot = Bot(token=os.environ.get("TOKEN"))
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=RedisStorage(redis=redis))
+
+button_callback = CallbackData("button", "text")
 
 
 @dp.message_handler(commands=["start"])
@@ -122,9 +131,9 @@ async def handler(message: types.Message):
         print(translate_message(message.text))
         if parser.__class__.__name__ == "PartialMatch":
             for i in parser.parse():
-                # Create a callback data string with the format "answer|<text>|<link>"
-                callback_data = f"answer|{i[1][0]}|{i[0]}"
-                keyboard.add(InlineKeyboardButton(text=i[0], callback_data=callback_data))
+                keyboard.add(InlineKeyboardButton(text=i[0],
+                                                  callback_data=button_callback.new(text=f"{i[1][0]}")))
+                                                                                         # f"\n<i>{hlink(i[0], i[1][1])}</i>")))
             await message.answer("Here are the results:", reply_markup=keyboard)
         else:
             for i in parser.parse():
@@ -135,12 +144,11 @@ async def handler(message: types.Message):
         await bot.send_message(chat_id=user_id, text=str(e))
 
 
-@dp.callback_query_handler(lambda c: c.data.startswith('answer|'))
+@dp.callback_query_handler()
 async def answer(call):
     callback_data = call.data
     # Split the callback data string and retrieve the text and link values
-    _, text, link = callback_data.split('|')
-    await call.message.answer(f"{text}\n<i>{link}</i>")
+    await call.message.answer(callback_data)
     await call.answer()
 
 
